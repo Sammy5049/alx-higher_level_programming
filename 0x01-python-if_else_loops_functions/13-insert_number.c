0c78f3f84edb6b299ab7f18b198ddd4cf2dff175
Damilola Adeola <samadedammy@gmail.com>
2023-09-06 11:02:43 +0100

    13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 
+
 /**
- * insert_node - func to insert node to orgnized list
- *	@head: the node list head
- * @number: postion on list to add the node
- * Return: NULL If the function fails and point to node if not
+ * insert_node - func to insert node into an ordered list
+ * @head: pointer to the head of the list
+ * @number: value to insert
+ * Return: pointer to the inserted node, or NULL on failure
  */
 listint_t *insert_node(listint_t **head, int number)
 {
@@ -12,7 +13,9 @@ listint_t *insert_node(listint_t **head, int number)
 
  added = malloc(sizeof(listint_t));
  if (added == NULL)
+ {
 		return (NULL);
+ }
  added->n = number;
 
  if (nPosition == NULL || number < nPosition->n)
@@ -23,7 +26,9 @@ listint_t *insert_node(listint_t **head, int number)
  }
 
  while (nPosition && nPosition->next && nPosition->next->n < number)
+ {
 		nPosition = nPosition->next;
+ }
 
  added->next = nPosition->next;
  nPosition->next = added;
