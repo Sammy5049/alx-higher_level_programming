1c2141d78ab9a0835eeaefc2232ea3ca8b51db49
Damilola Adeola <samadedammy@gmail.com>
2023-09-06 04:56:29 +0100

    12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def fizzbuzz():
+ for numb in range(1, 101):
+  if numb % 3 == 0:
+   print("fizz", end=" ")
+  elif numb % 5 == 0:
+   print("buzz", end=" ")
+  elif numb % 3 == 0 and numb % 5 == 0:
+   print("fizzbuzz", end=" ")
+
+  else:
+   print(numb, end=" ")
