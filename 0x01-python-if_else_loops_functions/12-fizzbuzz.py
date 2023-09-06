ef1bdfdfc8f4a44c18a514e245fad8ec133e70ff
Damilola Adeola <samadedammy@gmail.com>
2023-09-06 10:17:36 +0100

    indent

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 def fizzbuzz():
- for numb in range(1, 101):
-  if numb % 3 == 0:
-   print("fizz", end=" ")
-  elif numb % 5 == 0:
-   print("buzz", end=" ")
-  elif numb % 3 == 0 and numb % 5 == 0:
-   print("fizzbuzz", end=" ")
+    for numb in range(1, 101):
+        if numb % 3 == 0:
+            print("fizz", end=" ")
+        elif numb % 5 == 0:
+            print("buzz", end=" ")
+        elif numb % 3 == 0 and numb % 5 == 0:
+            print("fizzbuzz", end=" ")
 
-  else:
-   print(numb, end=" ")
+        else:
+            print(numb, end=" ")
