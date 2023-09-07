adaa1f3e5f5a38eaff3984ed144b6b17d2c7f824
Damilola Adeola <samadedammy@gmail.com>
2023-09-07 17:41:49 +0100

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-import argparse
-import sys
 
-length = len(sys.argv)
 
-if length - 1 == 0:
-    print("0 arguments.")
-elif length - 1 == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(length - 1))
-for i in range(length - 1):
-       print("{}: {}".format(i + 1, sys.argv[i + 1]))
+def main():
+    import sys
+
+    length = len(sys.argv)
+
+    if length - 1 == 0:
+        print("0 arguments.")
+    elif length - 1 == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(length - 1))
+    for i in range(length - 1):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
+
+if __name__ == "__main__":
+    main()
