b20e3605ca20389a9d3c8259a461d70984499982
Damilola <samadedammy@gmail.com>
2023-09-13 16:26:56 -0700

    1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    my_new_list = []
+    for i in my_list:
+        if i == search:
+            my_new_list.append(replace)
+        else:
+            my_new_list.append(i)
+        
+    return (my_new_list)
