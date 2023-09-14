459090fbba7ba681b6524951ccaddd93be8b4598
Damilola <samadedammy@gmail.com>
2023-09-13 22:04:26 -0700

    jkajdf

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    delete_val = [key for key, valued in a_dictionary.items() if valued == value]
+    delete_val = [key for key, val in a_dictionary.items() if val == value]
 
     for key in delete_val:
         del a_dictionary[key]
