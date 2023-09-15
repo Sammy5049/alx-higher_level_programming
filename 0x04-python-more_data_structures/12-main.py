a886439486fd51d3352e7a38c54bc6242c4b95ab
Damilola <samadedammy@gmail.com>
2023-09-15 09:31:20 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-main.py
+++ 0x04-python-more_data_structures/12-main.py
@@ -17,3 +17,5 @@ print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
 roman_number = "DCCVII"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+roman_number = "XX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None and not roman_string:
+    if type(roman_string) != str or not roman_string:
         return 0
 
     roman_numeral = {
