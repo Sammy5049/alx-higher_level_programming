c9f0c412e1b0868a6066cc53728104a49c22e5ff
Damilola <samadedammy@gmail.com>
2023-09-15 08:53:22 -0700

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None and not roman_string:
+        return 0
+
+    roman_numeral = {
+            "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000
+            }
+
+    result = 0
+
+    value_prev = 0
+
+    for num_key in roman_string[::-1]:
+        value = roman_numeral[num_key]
+
+        if value_prev > value:
+            result -= value
+        else:
+            result += value
+        value_prev = value
+    return result
