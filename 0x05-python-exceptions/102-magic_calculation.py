f86bb7e88e89bd1b211c7b4e8758239e9311726f
Damilola <samadedammy@gmail.com>
2023-09-27 09:04:16 -0700

    102-magic_calculation.py

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def magic_calculation(a, b):
+    res = 0
+    for j in range(1, 3):
+        try:
+            if j > a:
+                raise Exception('Too far')
+            else:
+                res += a ** b / j
+        except TypeError:
+            res = b + a
+            break
+    return (res)
