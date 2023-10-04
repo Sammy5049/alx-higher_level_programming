d23d859064f2b684ea4f254c87ddab11a467015c
Damilola <samadedammy@gmail.com>
2023-10-03 17:38:43 -0700

    afavsdfv

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,6 +6,13 @@
 
 class Rectangle:
     def __init__(self, width=0, height=0):
+
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
         self._width = width  """ Private instance attribute for width """
         self._height = height  """Private instance attribute for height"""
 
