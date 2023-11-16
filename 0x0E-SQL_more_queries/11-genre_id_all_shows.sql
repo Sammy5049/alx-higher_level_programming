247f1b54d0f002c33c83b9455a039d2c858a15a3
Adeola Damilola <samadedammy@gmail.com>
2023-11-16 19:05:41 +0100

    101-not_a_comedy.sql

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- Lists all shows contained in hbtn_0d_tvshows database.
+-- Show NULL for shows without genres.
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
+ 
