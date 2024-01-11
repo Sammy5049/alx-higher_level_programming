d2a255d7f9751803c4972bce854a88af31e8b6e5
Damilola <samadedammy@gmail.com>
2024-01-11 15:04:58 -0800

    4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Take in URL, add header variable; Usage: ./4-header.sh 0.0.0.0:5000/route_5 ; echo ""
-curl -sH "X-School-User-Id: 98" "http://0.0.0.0:5000/route_5/$1"
+# Take in URL, add header variable
 
+curl -s -H "X-School-User-Id":98 "$1"
